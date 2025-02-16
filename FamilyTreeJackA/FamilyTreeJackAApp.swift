f5ce7993233a7be0875e13b69de2918e1a60d5bc
JackAnlauf <prentrepreneur29@gmail.com>
2025-02-16 12:06:50 -0600

    Initial Commit

---
+++ FamilyTreeJackA/FamilyTreeJackAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTreeJackAApp.swift
+//  FamilyTreeJackA
+//
+//  Created by student on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTreeJackAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
